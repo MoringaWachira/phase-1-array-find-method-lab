43844236f3e079c63f8c9b9db86c69c8bfbb46ad
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-20 17:49:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code your solution here
+// code your solution here //...
+
+
+const record = [
+    {
+     year: "2015", 
+     result: "W"
+    },
+     
+    {
+    year: "2014",
+     result: "N/A"
+    },
+
+    {
+     year: "2013", 
+     result: "L"
+    },
+    
+  ];
+
+
+
+  function superbowlWin(array) {
+    let arrayRecord = array.find(function (storeResult) {
+        return storeResult.result === "W"
+    });
+    if (typeof arrayRecord === "object") {
+        return arrayRecord.year;
+    }
+}
+
+superbowlWin(record);
